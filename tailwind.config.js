8d4b51831f43191db2a51c4df3662a2f55613ff8
YohanaSriRejeki <maria.yohanasrirejeki@gmail.com>
2022-05-23 22:10:39 +0700

    Update The Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screen: {
+        '2xl' : '1320px'
+      },
+    },
+  },
+  plugins: [],
+}
